761e728d4563d07ad09233abbef44ee08faa1e19
Alina Kozlova <80878732+aokozlova@users.noreply.github.com>
2025-11-08 07:08:12 +0100

    Update 00_eda_decisions.sql
    fixed typos, added comments

--- 00_eda/00_eda_decisions.sql
+++ 00_eda/00_eda_decisions.sql
@@ -1,122 +1,156 @@
 SELECT * FROM  `abtest_raw.desicions`
 LIMIT 5;
 
---checkig nulls
---no nulls in experiment or variation
-SELECT COUNT(*) as experiment_id_nulls FROM `abtest_raw.desicions`
-WHERE experiment_id is NULL;
 
-SELECT COUNT(*) as variation_id_nulls FROM `abtest_raw.desicions`
-WHERE variation_id is NULL;
+--checking nulls
+--no nulls
+SELECT COUNT(*) AS total_cnt,
+      SUM(CASE WHEN experiment_id IS NULL THEN 1 ELSE 0 END) as experiment_nulls,
+      SUM(CASE WHEN variation_id IS NULL THEN 1 ELSE 0 END) as variation_nulls,
+      SUM(CASE WHEN visitor_id IS NULL THEN 1 ELSE 0 END) as visitor_nulls,
+      SUM(CASE WHEN session_id IS NULL THEN 1 ELSE 0 END) as session_nulls,
+      SUM(CASE WHEN date IS NULL THEN 1 ELSE 0 END) as date_value_nulls,
+      SUM(CASE WHEN timestamp IS NULL THEN 1 ELSE 0 END) as timestamp_nulls,
+      SUM(CASE WHEN user_agent IS NULL THEN 1 ELSE 0 END) as user_agent_nulls
+FROM `abtest_raw.desicions`;
+
+
+
 
 --checking if we have data from another experiment
---only one experiment
+--we have one experiment id
 SELECT DISTINCT experiment_id FROM `abtest_raw.desicions`;
+
 
 --variation distribution
 --~50/50
-SELECT COUNT(*) AS cnt FROM `abtest_raw.desicions`
+SELECT COUNT(*) AS record_cnt FROM `abtest_raw.desicions`
 GROUP BY variation_id;
+
 
 --visitors have multiple sessions and timestamps
 SELECT variation_id,
-       COUNT(DISTINCT visitor_id) as visitors, 
-       COUNT(DISTINCT session_id) as sessions,
-       COUNT(DISTINCT timestamp)  as timestamp_,
-       COUNT(*) AS total_rows
+      COUNT(DISTINCT visitor_id) as visitors,
+      COUNT(DISTINCT session_id) as sessions,
+      COUNT(DISTINCT timestamp)  as timestamp_,
+      COUNT(*) AS record_cnt
 FROM `abtest_raw.desicions`
 GROUP BY variation_id;
 
---checking duplicates 
---!! we have duplicates
+
+--checking duplicates by visitor, session, timestamp
+--!! We have 4 duplicates
 SELECT visitor_id,
-       session_id,
-       timestamp,
-       COUNT(*) as cnt
+      session_id,
+      timestamp,
+      COUNT(*) as cnt
 FROM `abtest_raw.desicions`
 GROUP BY 1,2,3
 HAVING cnt > 1;
 
---are there same users or session in both variations
+
+--are there the same users or sessions in both variations
 --not found
-SELECT visitor_id, 
-       COUNT(DISTINCT variation_id) as variations
+SELECT visitor_id,
+      COUNT(DISTINCT variation_id) as variations
 FROM `abtest_raw.desicions`
 GROUP BY visitor_id
 HAVING variations > 1;
 
-SELECT session_id, 
-       COUNT(DISTINCT variation_id) as variations
+
+SELECT session_id,
+      COUNT(DISTINCT variation_id) as variations
 FROM `abtest_raw.desicions`
 GROUP BY session_id
 HAVING variations > 1;
 
---how many sessions and users agents visitor can have
+
+--how many sessions and user agents a visitor can have
 --visitors can have multiple user_agents, sessions and timestamps
 SELECT visitor_id,
-       COUNT(DISTINCT user_agent) as user_agent_cnt,
-       COUNT(DISTINCT session_id) as sessions_cnt,
-       COUNT(DISTINCT timestamp) as timestamp_cnt
+      COUNT(DISTINCT user_agent) as user_agent_cnt,
+      COUNT(DISTINCT session_id) as sessions_cnt,
+      COUNT(DISTINCT timestamp) as timestamp_cnt
 FROM `abtest_raw.desicions`
 GROUP BY visitor_id
 ORDER BY 2 DESC,3 DESC,4 DESC;
 
+
 --checking timerange
+--timestamp 26-10-2022 - 21-12-2022
+--date 10-11-2022 - 13-11-2022
 SELECT MIN(timestamp) as min_timestamp,
-       MAX(timestamp) as max_timestamp
+      MAX(timestamp) as max_timestamp,
+      MIN(date) as min_date,
+      MAX(DATE) as max_date
 FROM `abtest_raw.desicions`;
 
 
---group data for visualization (go to Looker stuido)
+
+
+--group data for visualisation (go to Looker studio)
 --https://lookerstudio.google.com/u/1/reporting/fe30b51d-2496-4a8f-b9a9-cb2790d1ab90/page/tEnnC/edit
--- majority of visitors had timestamp at the same date as creatuon date
--- some visotrs have timestamp earlier than creation date
+-- The majority of visitors had a timestamp on the same date as the creation date
+-- Some visitors have a timestamp earlier than the creation date
 SELECT DATE(timestamp) as desicion_date,
-       date,
-       variation_id,
-       COUNT(DISTINCT visitor_id) as visitors,
-       COUNT(DISTINCT session_id) as session
+      date,
+      variation_id,
+      COUNT(DISTINCT visitor_id) as visitors,
+      COUNT(DISTINCT session_id) as session
 FROM `abtest_raw.desicions`
 GROUP BY DATE(timestamp),
-          date,
-         variation_id;
+         date,
+        variation_id;
 
---how many rows do we have with mismatching tamestap vs creation date
+
+--How many rows do we have with mismatching timestamp vs creation date
+--in total 531 before and 4 after creation date
 SELECT
-  COUNT(*) AS total_rows,
-  SUM(CASE WHEN DATE(timestamp) > date THEN 1 ELSE 0 END) AS after_creation,
-  SUM(CASE WHEN DATE(timestamp) = date THEN 1 ELSE 0 END) AS same_day,
-  SUM(CASE WHEN DATE(timestamp) < date THEN 1 ELSE 0 END) AS before_creation
+ COUNT(*) AS total_rows,
+ SUM(CASE WHEN DATE(timestamp) > date THEN 1 ELSE 0 END) AS after_creation,
+ SUM(CASE WHEN DATE(timestamp) = date THEN 1 ELSE 0 END) AS same_day,
+ SUM(CASE WHEN DATE(timestamp) < date THEN 1 ELSE 0 END) AS before_creation
 FROM `abtest_raw.desicions`;
 
+
 --
-SELECT * 
+SELECT *
 FROM `abtest_raw.desicions`
 WHERE DATE(timestamp) <> date;
 
---how many unique user agent we have?
+
+--How many unique user agents do we have?
+--3023
 SELECT COUNT(DISTINCT user_agent) as user_agent_cnt
 FROM `abtest_raw.desicions`;
 
---what are the most popular user agens?
-SELECT user_agent, 
-       COUNT(*) AS  records_cnt
+
+--What are the most popular user agents?
+--We will be able to extract the platform device type and iOS
+SELECT user_agent,
+      COUNT(*) AS  records_cnt
 FROM `abtest_raw.desicions`
 GROUP BY user_agent
-ORDER BY 2 desc;
+ORDER BY 2 desc
+LIMIT 10;
 
 
 
---do the most popular user agents have the 50/50 distribution by variations?
-SELECT user_agent, 
-       variation_id,
-       COUNT(*) as records_cnt
+
+
+
+--Do the most popular user agents have a 50/50 distribution by variations?
+--yes
+SELECT user_agent,
+      variation_id,
+      COUNT(*) as records_cnt
 FROM `abtest_raw.desicions`
 GROUP BY user_agent, variation_id
 ORDER BY records_cnt desc
 Limit 10;
 
-/*each user can visit our page from multiple user agents. I want you to allocate each user to the user agent with the highest total number of occurrences (rows in the decisions table)
+
+/*Each user can visit our page from multiple user agents. I want you to allocate each user to the user agent with the highest total number of occurrences (rows in the decisions table)
 
 Example:
 Sara visited from user_agent_x and user_agent_y. User_agent_x AMONG ALL PLAYERS (not just for Sara) has 100 conversions,  User_agent_y has 50. Sara should be attributed to user_agent_x.
@@ -124,7 +158,5 @@ Sara visited from user_agent_x and user_agent_y. User_agent_x AMONG ALL PLAYERS 
 
 
 
-
-         
 
 
